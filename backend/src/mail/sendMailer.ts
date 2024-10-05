327123f92b381f99c8da46e6558244d77b25dc27
davinavarro998 <davinavarro@2ndmarket.com.br>
2024-10-05 10:12:40 -0300

    lets improve the smart contract

--- backend/src/mail/sendMailer.ts
+++ backend/src/mail/sendMailer.ts
@@ -7,39 +7,6 @@ const SMTP_PORT:number = parseInt(`${process.env.SMTP_PORT}`);
 const SMTP_USERNAME:string = `${process.env.SMTP_USERNAME}`;
 const SMTP_PASSWORD:string = `${process.env.SMTP_PASSWORD}`;
 
-/**
- * 
- * const nodemailer = require("nodemailer");
-
-async function sendEmail() {
-  try {
-    const transporter = nodemailer.createTransport({
-      service: "Gmail",
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
-      auth: {
-        user: "davialbuquerque998@gmail.com",
-        pass: "iltg kdms nqid knme",
-      },
-    });
-    const mailOptions = {
-      from: "davialbuquerque998@gmail.com",
-      to: "davialbuquerque998@gmail.com",
-      subject: "Hello from Nodemailer",
-      text: "This is a test email sent using Nodemailer.",
-    };
-
-    let info = await transporter.sendMail(mailOptions);
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  } catch (error) {
-    console.error("Error: ", error);
-  }
-}
-
-sendEmail();
- */
 export default async function emailSender(name:string, email:string, to:string, subject:string, message:string) {
     const smtpTransport = nodemailer.createTransport({
         host:SMTP_SERVER,
